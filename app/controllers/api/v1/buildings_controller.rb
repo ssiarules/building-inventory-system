405970ac115ebeb6e3d12ad2e5d7c24d694c5aea
JOHNSON JOSEPH <johnsonjoseph@JOHNSONs-MacBook-Air.local>
2020-01-11 17:54:29 -0800

    create api, active rake-cors, created building controller, routes, create migration

---
+++ app/controllers/api/v1/buildings_controller.rb
@@ -0,0 +1,40 @@
+class Api::V1::BuildingsController < ApplicationController
+    def index
+        @buildings = Building.all
+
+        render json: @building, status: 200 
+    end 
+
+    def show 
+        @building = Building.find(params[:id])
+
+        render json: @building, status: 200 
+    end 
+    
+    def create 
+        @building = Building.create(building_params)
+
+        render json: @building, status: 200 
+    end 
+
+    def update 
+        @building = Building.find(params[:id])
+         @building.update(building_params)
+
+        render json: @building, status: 200 
+     end 
+
+     def destroy
+         @building = Product.find(params[:id])
+         @building.delete 
+
+        render json: { buildingId: @building.id}
+     end 
+
+    private
+
+    def building_params
+     params.require(:building).permit(:name)
+    end 
+
+end
